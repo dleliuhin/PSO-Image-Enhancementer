6091823c2e0143c4e41fe8a59868d1b93c303420
Dmitrii Leliuhin <dleliuhin@mail.ru>
2018-12-07 11:17:20 +0300

    Initial commit
    This is where it all begins...

---
+++ mainPso.m
@@ -0,0 +1,37 @@
+function mainPso()
+%% MAINPSO 
+% Program to enhance image quality based on 
+% Particle Swarm Optimization Alghoritm.
+% 
+% * Syntax 
+% 
+%	[] = MAINPSO()  
+% 
+% * Examples: 
+% 
+% Provide sample usage code here
+% 
+% * See also: 
+% 
+% List related files here 
+% 
+% * Author: Dmitrii Leliuhin 
+% * Email: dleliuhin@mail.ru 
+% * Date: 07/12/2018 11:04:25 
+% * Version: 1.0 $ 
+% * Requirements: PCWIN64, MatLab R2016a 
+% 
+% * Warning: 
+% 
+% # Warnings list. 
+% 
+% * TODO: 
+% 
+% # TODO list. 
+% 
+
+%% Code 
+
+
+
+end
